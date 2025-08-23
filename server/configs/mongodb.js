9f4dc37a48b14da71bcb1e1c1b525fd87211bc6d
unknown <khajuriaraghu0@gmail.com>
2025-08-24 00:36:10 +0530

    db

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,13 +1,30 @@
+// import mongoose from "mongoose";
+
+// // connected to mongodb database
+
+// const connectDB = async () => {
+//     mongoose.connection.on('connected', () => {
+//         console.log('DataBase Connected');
+//     })
+
+//     await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
+// }
+
+// export default connectDB
+
 import mongoose from "mongoose";
 
-// connected to mongodb database
-
+// connect to mongodb database
 const connectDB = async () => {
-    mongoose.connection.on('connected', () => {
-        console.log('DataBase Connected');
-    })
+  mongoose.connection.on("connected", () => {
+    console.log("✅ MongoDB Connected:", mongoose.connection.name);
+  });
 
-    await mongoose.connect(`${process.env.MONGODB_URL}/lms`)
-}
+  await mongoose.connect(process.env.MONGODB_URL, {
+    dbName: "lms", // 👈 this is the actual database name
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+};
 
-export default connectDB
+export default connectDB;
